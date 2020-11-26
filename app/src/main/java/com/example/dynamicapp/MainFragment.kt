b5a2c4457c6d60944432306a7d1e00fa05bd5d25
Sergei Kurikalov <skurikalov@scnsoft.com>
2020-11-26 13:31:44 +0300

    Dynamic Nav graph Progress

--- app/src/main/java/com/example/dynamicapp/MainFragment.kt
+++ app/src/main/java/com/example/dynamicapp/MainFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.navigation.dynamicfeatures.DynamicExtras
+import androidx.navigation.dynamicfeatures.DynamicInstallMonitor
 import androidx.navigation.fragment.findNavController
+import com.google.android.play.core.splitinstall.SplitInstallSessionState
+import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus.*
 import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
@@ -23,7 +29,46 @@ class MainFragment : Fragment() {
 
     private fun bindUseModuleButton() {
         useModule.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_onDemandFragment)
+            openDynamicModule()
         }
+    }
+
+    private fun openDynamicModule() {
+        val installMonitor = DynamicInstallMonitor()
+        findNavController().navigate(
+            R.id.action_mainFragment_to_onDemandFragment,
+            null,
+            null,
+            DynamicExtras(installMonitor)
+        )
+
+        if (installMonitor.isInstallRequired) {
+            observeInstallDynamicModule(installMonitor)
+        }
+    }
+
+    private fun observeInstallDynamicModule(installMonitor: DynamicInstallMonitor) {
+        installMonitor.status.observe(
+            viewLifecycleOwner,
+            object : Observer<SplitInstallSessionState> {
+                override fun onChanged(state: SplitInstallSessionState) {
+                    getSessionStateListener(state)
+                    if (state.hasTerminalStatus()) {
+                        installMonitor.status.removeObserver(this)
+                    }
+                }
+            })
+    }
+
+    private fun getSessionStateListener(state: SplitInstallSessionState) {
+        when (state.status()) {
+            DOWNLOADING -> showToast("Downloading feature")
+            FAILED -> showToast("Install failed (${state.errorCode()})")
+            INSTALLED -> showToast("OnDemand module is ready to be used")
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 }
